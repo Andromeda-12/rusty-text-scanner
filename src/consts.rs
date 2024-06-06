536cd03fa264573d587610b87e37048ab18f871e
Andromeda <andrei.froschgaizer@yandex.ru>
2024-06-06 22:26:06 +0700

    update consts

--- src/consts.rs
+++ src/consts.rs
@@ -1,6 +1,11 @@
 pub mod colors {
     use termcolor::Color;
 
-    pub const GRAY: Color = Color::Ansi256(8);
-    pub const YELLOW: Color = Color::Yellow;
+    const GRAY: Color = Color::Ansi256(8);
+    const LIGHT_GRAY: Color = Color::Ansi256(255); // поменять
+    const YELLOW: Color = Color::Yellow;
+
+    pub const HIGHLIGHTED_WORD_BG_COLOR: Color = YELLOW;
+    pub const HIGHLIGHTED_LINE_FG_COLOR: Color = LIGHT_GRAY;
+    pub const ADDITIONAL_LINE_FG_COLOR: Color = GRAY;
 }
