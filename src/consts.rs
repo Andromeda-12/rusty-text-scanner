a4cb7467897bb79e9bb2e695a18d1d7049d9fdf3
Andromeda <andrei.froschgaizer@yandex.ru>
2024-07-02 21:59:19 +0700

    update consts

--- src/consts.rs
+++ src/consts.rs
@@ -1,11 +1,15 @@
-pub mod colors {
-    use termcolor::Color;
+pub mod consts {
+    pub const CHUNK_SIZE: usize = 2;
 
-    const GRAY: Color = Color::Ansi256(8);
-    const LIGHT_GRAY: Color = Color::Ansi256(255); // поменять
-    const YELLOW: Color = Color::Yellow;
+    pub mod colors {
+        use termcolor::Color;
 
-    pub const HIGHLIGHTED_WORD_BG_COLOR: Color = YELLOW;
-    pub const HIGHLIGHTED_LINE_FG_COLOR: Color = LIGHT_GRAY;
-    pub const ADDITIONAL_LINE_FG_COLOR: Color = GRAY;
+        const GRAY: Color = Color::Ansi256(8);
+        const LIGHT_GRAY: Color = Color::Ansi256(255); // поменять
+        const YELLOW: Color = Color::Yellow;
+
+        pub const HIGHLIGHTED_WORD_BG_COLOR: Color = YELLOW;
+        pub const HIGHLIGHTED_LINE_FG_COLOR: Color = LIGHT_GRAY;
+        pub const LINE_NUMBER_FG_COLOR: Color = GRAY;
+    }
 }
