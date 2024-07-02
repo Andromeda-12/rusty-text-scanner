5f60846cdf1c9032e5989d798a8a1bbb9f93f9e4
Andromeda <andrei.froschgaizer@yandex.ru>
2024-07-02 22:00:26 +0700

    update main

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,43 @@
-mod color_utils;
+use std::env;
+
+use config::Config;
+use consts::consts::CHUNK_SIZE;
+use reader::FileReader;
+use searcher::Searcher;
+use types::{SearcherTrait, WriterTrait};
+use writer::Writer;
+
+mod config;
 mod consts;
-mod test;
-mod utils;
+mod reader;
+mod searcher;
+mod types;
+mod writer;
 
-fn main() {
-    color_utils::print_matched_line(
-        "ТЕСТ нетест sss".to_owned(),
-        vec!["1: test".to_string(), "2: test".to_string()],
-        vec!["4: test".to_string(), "5: test".to_string()],
-        "тест",
-        false,
-        true,
-    );
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Vec<String> = env::args().collect();
+    let config = Config::new(&args)?;
 
-    // let mut stdout = StandardStream::stdout(termcolor::ColorChoice::Always);
-    // let mut color_spec = ColorSpec::new();
+    let mut reader = FileReader::new(config.file_path)?;
+    let searcher = Searcher::new(config.search_mode, config.case_sensitive);
+    let mut writer = Writer::new();
 
-    // for i in 1..255 {
-    //     color_spec.set_fg(Some(Color::Ansi256(i)));
-    //     stdout.set_color(&color_spec);
-    //     let _ = stdout.write("text\n".as_bytes());
-    // }
+    let mut iteration = 0;
+    loop {
+        let lines = reader.read_next_chunk()?;
+
+        let res = searcher.search(&lines, &config.search_word, iteration);
+
+        for r in &res {
+            writer.print_painted_line_with_highlighted_substring(r)?;
+        }
+
+        if lines.len() < CHUNK_SIZE {
+            break;
+        }
+
+        iteration += 1;
+    }
+
+    Ok(())
 }
